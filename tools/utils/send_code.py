733c3c145266053e16e89c7e6e041f54df30d0af
Erfan <erfanmoshiri99@gmail.com>
2023-02-01 11:21:03 +0330

    fixed sms sending

--- tools/utils/send_code.py
+++ tools/utils/send_code.py
@@ -5,4 +5,4 @@ def send_code(receptor: str, code):
     msg = f'سلام.\r\nکد ورود شما : {code}'
 
     sms = ghasedakpack.Ghasedak('5216b510de17fd67130c68970b835af4c9356fff42f1f83b05376f9e36d6a3ce')
-    sms.send({'message': msg, 'receptor': receptor, 'linenumber': '300002525'})
+    sms.send({'message': msg, 'receptor': receptor, 'linenumber': '50001212125380'})
